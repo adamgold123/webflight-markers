11ad615f5554148fdbe42c6d40226c397b90a1e3
Gary Anderson <ganderson@seattleacademy.org>
2017-02-20 18:06:16 -0800

    randal pose in individual spans

--- public/js/markers.js
+++ public/js/markers.js
@@ -5,10 +5,19 @@
         console.log("Loading markers plugin.");
         this.cockpit = cockpit;
 
-        $('.main-container .wrapper').append("<div id='markers'>...</div>");
+        $('.main-container .wrapper').append("<div id='markers'></div>");
 
         $('#markers').append('<button id="markerson">Markers On</button>');
         $('#markers').append('<div>counter: <span id="counter">0</span></div>');
+        $('#markers').append('<div>nbDetected: <span id="nbDetected">0</span></div>');
+        $('#markers').append('<div>type: <span id="type">0</span></div>');
+        $('#markers').append('<div>xc: <span id="xc">0</span></div>');
+        $('#markers').append('<div>yc: <span id="yc">0</span></div>');
+        $('#markers').append('<div>width: <span id="width">0</span></div>');
+        $('#markers').append('<div>height: <span id="height">0</span></div>');
+        $('#markers').append('<div>dist: <span id="dist">0</span></div>');
+        $('#markers').append('<div>orientationAngle: <span id="orientationAngle">0</span></div>');
+        $('#markers').append('<div>vision: <span id="vision">0</span></div>');
 
         this.listen();
 
@@ -16,14 +25,23 @@
 
         this.cockpit.socket.on('navdata', function(data) {
             //$("#status").text(data.sequenceNumber);
-            $("#counter").text(data.sequenceNumber + JSON.stringify(data.visionDetect, null, 4));
+            $("#counter").text(data.sequenceNumber);
+            $("#nbDetected").text(data.visionDetect.nbDetected);
+            $("#type").text(data.visionDetect.type);
+            $("#xc").text(data.visionDetect.xc);
+            $("#yc").text(data.visionDetect.yc);
+            $("#width").text(data.visionDetect.width);
+            $("#height").text(data.visionDetect.height);
+            $("#dist").text(data.visionDetect.dist);
+            $("#orientationAngle").text(data.visionDetect.orientationAngle[0].toFixed(1));
+            //$("#vision").text(JSON.stringify(data.visionDetect, null, 4));
             //console.log(data.sequenceNumber);
         });
         this.listen();
     };
 
     Markers.prototype.listen = function listen() {
-    	console.log("markers listen")
+        console.log("markers listen")
         var markers = this;
 
         $('#markerson').click(function(ev) {
